a9c7ade13c2a5f999300e56c9b800237f032be55
Vinod Kumar <vk5241415@gmail.com>
2024-02-27 19:36:56 +0530

    meta fixed -1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 
+
 export const metadata: Metadata = {
   applicationName:"Vk Dev Studio - Innovative Solutions for Web and Mobile Development",
   // authors:"Vinod Kumar"
